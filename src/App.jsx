21f491023be69102dc67ad1f9d21cc3a4be0f0e2
Tacite243 <taciteiragi@gmail.com>
2024-06-05 14:22:03 +0100

    feat(setdate): create space of setintervalle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import SetDate from './components/setdate/setdate';
+
 function App() {
     return (
         <div>
             <h2>Most visited wikimedia articles</h2>
+            <SetDate />
         </div>
     );
 }
---
+++ src/components/setdate/setdate.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function SetDate() {
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    return (
+        <div className='setInterval'>
+            <h6>Choisir un intervalle</h6>
+            <div>
+                <span className='label' htmlFor='startDate'>
+                    Date de debut
+                </span>
+                <input type='date' name='startDate' value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
+            </div>
+            <div>
+                <span className='label' htmlFor='endDate'>
+                    Date de fin
+                </span>
+                <input type='date' name='endDate' value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
+            </div>
+        </div>
+    );
+}
