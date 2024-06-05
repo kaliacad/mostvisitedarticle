32bb7f9dbd3a0549ef9dc31707bba29176773246
firmin nagnduli <finganduli@gmail.com>
2024-06-05 16:10:10 +0200

    feat: added reusable button with prop text and event

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,14 +5,8 @@ const defaultEvent = () => {
 };
 export default function Button({ text, event }) {
     const text2 = "You have to add a prop 'text'";
-    if (event) {
-        return <button onClick={defaultEvent()}>{text || text2}</button>;
-    } else {
-        if (text) return <button onClick={event || defaultEvent}>{text}</button>;
-        else {
-            return <button onClick={event || defaultEvent}>{text2}</button>;
-        }
-    }
+
+    return <button onClick={defaultEvent() || event}>{text || text2}</button>;
 }
 
 Button.propTypes = {
