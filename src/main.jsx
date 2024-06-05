9289e8055f20374383c6a3b4bfe26d9a9fd1a47f
PatriceRoot <patricekalwira@gmail.com>
2024-06-05 16:20:36 +0200

    feat: update codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Loading from './components/loading.jsx';
 import './global.css';
+import Loading from './components/loading.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
