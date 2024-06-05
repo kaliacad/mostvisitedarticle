5fd7f07d6129fd7c030434c47b6188607ad11cff
Tacite243 <taciteiragi@gmail.com>
2024-06-05 16:03:19 +0100

    fix(setdate): initialise default date

--- src/components/setdate/setdate.jsx
+++ src/components/setdate/setdate.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 
 export default function SetDate() {
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    const today = new Date();
+    today.setDate(today.getDate());
+    const [intervalDate, setIntervalDate] = useState({
+        startDate: date.toISOString().split('T')[0],
+        endDate: today.toISOString().split('T')[0],
+    });
+    const handleChange = (e) => {
+        setIntervalDate({ ...intervalDate, [e.target.name]: e.target.value });
+    };
     return (
         <div className='setdate'>
             <h6>Choisir un intervalle</h6>
@@ -10,13 +19,13 @@ export default function SetDate() {
                 <span className='label' htmlFor='startDate'>
                     Date de debut
                 </span>
-                <input type='date' name='startDate' value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
+                <input type='date' name='startDate' defaultValue={intervalDate.startDate} onChange={handleChange} required />
             </div>
             <div>
                 <span className='label' htmlFor='endDate'>
                     Date de fin
                 </span>
-                <input type='date' name='endDate' value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
+                <input type='date' name='endDate' defaultValue={intervalDate.endDate} onChange={handleChange} required />
             </div>
         </div>
     );
