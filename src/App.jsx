6425d810caec8ba4f6d47646d5cc52c0a29db63d
Gerard <gerardcubakabisimwa@gmail.com>
2024-06-05 16:22:20 +0200

    feat: ajout du composant ArticleCard réutilisable et du style articlecard.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import ArticleCard from './components/ArticleCard';
 function App() {
     return (
         <div>
             <h2>Most visited wikimedia articles</h2>
+            <ArticleCard />
         </div>
     );
 }
