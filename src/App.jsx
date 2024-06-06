8c558a81482f6a77d1e270a655884725bce6b64e
Ezechiel Mukenge <ezechielmukenge572@gmail.com>
2024-06-06 14:05:23 +0200

    feat(article): get from article

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import SetDate from './components/setdate/setdate';
 import MostArticleByProject from './components/MostArticleByProject';
 import Button from './components/Button.jsx';
 import WikiAfricaTopArticles from './components/africanArticlesList.jsx';
+import ResultListArticles from './components/ResultListArticles.jsx';
 function App() {
     const handleClick = () => {
         alert('Button clicked!');
     };
     return (
         <div>
+            <ResultListArticles />
             <h2>Most visited wikimedia articles</h2>
             <SetDate />
             <MostArticleByProject project='fr.wikipedia' />
