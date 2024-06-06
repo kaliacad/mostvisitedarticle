948a0a4395c14635b559d2f3a604b798a5ddce18
Tacite243 <taciteiragi@gmail.com>
2024-06-06 11:27:34 +0100

    feat(metadata): add metadata (title & description)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import SetDate from './components/setdate/setdate';
 import MostArticleByProject from './components/MostArticleByProject';
 import Button from './components/Button.jsx';
 import WikiAfricaTopArticles from './components/africanArticlesList.jsx';
+import Metadata from './components/metadata/metadata.jsx';
+
 function App() {
     const handleClick = () => {
         alert('Button clicked!');
     };
     return (
         <div>
+            <Metadata />
             <h2>Most visited wikimedia articles</h2>
             <SetDate />
             <MostArticleByProject project='fr.wikipedia' />
---
+++ src/components/metadata/metadata.jsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet';
+
+export default function Metadata() {
+    return (
+        <Helmet>
+            <title>Emingi</title>
+            <meta name='description' content='Soyez creatif' />
+        </Helmet>
+    );
+}
