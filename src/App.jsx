c9226516980685d3eb5558b214fff1cb9dc84247
paluku-ezechiel <55324814+paluku-ezechiel@users.noreply.github.com>
2024-06-06 14:51:01 +0200

    feat: creation menu deroulant (#93)

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,27 @@ import ResultatGallery from './components/ResultatGallery.jsx';
 import NavBar from './components/NavBar.jsx';
 import ArticleCard from './components/ArticleCard.jsx';
 import ExportDropdownButton from './components/DropdownExport.jsx';
+import { Menu } from './components/Menu';
+import { datab } from '../data';
 function App() {
+    const titre = 'Pays';
     const handleClick = () => {
         alert('Button clicked!');
     };
     return (
-            <div id='results'>
-                <ExportDropdownButton htmlId='results' fileName='results' />
-                <NavBar />
-                <ResultatGallery />
-                <ArticleCard />
-                <SetDate />
-                <MostArticleByProject project='fr.wikipedia' />
-                <Button text='submit' event={handleClick}></Button>
-                <WikiAfricaTopArticles />
-                <Footer />
-            </div>
+        <div>
+            <h2>Most visited wikimedia articles</h2>
+            <ResultatGallery />
+            <SetDate />
+            <Menu data={datab} title={titre} />
+            <MostArticleByProject project='fr.wikipedia' />
+            <Button text='submit' event={handleClick}></Button>
+            <WikiAfricaTopArticles />
+            <NavBar />
+            <ArticleCard />
+            <ExportDropdownButton />
+            <Footer />
+        </div>
     );
 }
 
