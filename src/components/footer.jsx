d184796a6ce7235de298929aae07e87cd4a50144
francetambwe <143693696+francetambwe@users.noreply.github.com>
2024-06-06 15:04:48 +0200

    feat(footer): add link to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,16 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export function Footer() {
     return (
         <footer className='footer'>
-            <a href='#' className='footer-link'>
+            <a href='https://meta.wikimedia.org/wiki/Emi_Solution' className='footer-link'>
                 <FontAwesomeIcon icon={faBook} /> Documentation
             </a>
             <a href='https://github.com/kaliacad/mostvisitedarticle' className='footer-link'>
                 <FontAwesomeIcon icon={faCode} /> View source
             </a>
-            <a href='#' className='footer-link'>
+            <a href='https://github.com/kaliacad/mostvisitedarticle/issues' className='footer-link'>
                 <FontAwesomeIcon icon={faBug} /> Report an issue
             </a>
-            <a href='#' className='footer-link'>
+            <a href='https://meta.wikimedia.org/w/index.php?title=Talk:Emi_Solution&action=edit&redlink=1' className='footer-link'>
                 <FontAwesomeIcon icon={faComment} /> Feedback
             </a>
             <a href='https://kaliacademy.org/' className='footer-link'>
