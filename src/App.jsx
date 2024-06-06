4752fefc6e8bc726b6a089ddba981e097bc5939e
BirushaNdegeya <birushandegeya@gmail.com>
2024-06-06 13:06:48 +0200

    fix: resultat en gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import ArticleCard from './components/ArticleCard';
 import SetDate from './components/setdate/setdate';
 import MostArticleByProject from './components/MostArticleByProject';
 import Button from './components/Button.jsx';
 import WikiAfricaTopArticles from './components/africanArticlesList.jsx';
 import { Footer } from './components/footer.jsx';
+import ResultatGallery from './components/ResultatGallery.jsx';
 function App() {
     const handleClick = () => {
         alert('Button clicked!');
@@ -11,7 +11,7 @@ function App() {
     return (
         <div>
             <h2>Most visited wikimedia articles</h2>
-            <ArticleCard />
+            <ResultatGallery />
             <SetDate />
             <MostArticleByProject project='fr.wikipedia' />
             <Button text='submit' event={handleClick}></Button>
