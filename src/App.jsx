47bd5a18cc47a58350aee80623d4346f79343770
landbit <landbit@debian.landry.com>
2024-06-06 14:02:09 +0200

    feat: add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Button from './components/Button.jsx';
 import WikiAfricaTopArticles from './components/africanArticlesList.jsx';
 import { Footer } from './components/footer.jsx';
 import ResultatGallery from './components/ResultatGallery.jsx';
+import NavBar from './components/NavBar.jsx';
+import ArticleCard from './components/ArticleCard.jsx';
 function App() {
     const handleClick = () => {
         alert('Button clicked!');
     };
     return (
         <div>
-            <h2>Most visited wikimedia articles</h2>
+            <NavBar />
             <ResultatGallery />
+            <ArticleCard />
             <SetDate />
             <MostArticleByProject project='fr.wikipedia' />
             <Button text='submit' event={handleClick}></Button>
