2cbc66836f90ed78ae43a02a38917e017b347c15
Ezechiel Mukenge <ezechielmukenge572@gmail.com>
2024-06-06 13:42:05 +0200

    refactor(ResultListArticles): fetch data from API instead of using placeholder data

--- src/components/ResultListArticles.jsx
+++ src/components/ResultListArticles.jsx
@@ -1,34 +1,24 @@
-const data = [
-    {
-        rank: 1,
-        article: 'Article 1',
-        views: 2849,
-    },
-    {
-        rank: 2,
-        article: 'Article 2',
-        views: 23,
-    },
-    {
-        rank: 3,
-        article: 'Article 3',
-        views: 3,
-    },
-    {
-        rank: 4,
-        article: 'Article 4',
-        views: 3,
-    },
-];
-
 // PLACEHOLDER DATA
 
+import { useEffect, useState } from 'react';
+import fetchTopArticles from '../api';
+
 export default function ResultListArticles() {
-    // props { rank, article, views }
-
-    // NOTE
-
-    // <ResulatList
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function laodAPI() {
+            const d = await fetchTopArticles({
+                countryCode: 'CD',
+                access: 'all-access',
+                year: '2023',
+                month: '07',
+                day: '01',
+            });
+            const resp = await d.items[0].articles;
+            setData(resp);
+        }
+        laodAPI();
+    }, []);
     return (
         <div>
             <div>
@@ -37,7 +27,8 @@ export default function ResultListArticles() {
                           <div key={article.rank} style={{ display: 'flex', alignItems: 'center', padding: 10, borderBottom: '1px solid #ccc' }}>
                               <span style={{ fontWeight: 'bold', marginRight: 10 }}>{article.rank}</span>
                               <span style={{ flex: 1 }}>{article.article}</span>
-                              <span style={{ fontSize: 14, color: '#666' }}>{article.views} views</span>
+                              <span>{article.project}</span>
+                              <span style={{ fontSize: 14, color: '#666' }}>{article.views_ceil} views</span>
                           </div>
                       ))
                     : null}
