cb41230e4299e54dc3a8dc6d3394518f5aa3e772
Benedicte Tuteka Mukuta <117514619+blessingtutka@users.noreply.github.com>
2024-06-07 12:42:15 +0200

    feat: add graph page views  (#135)
    * feat: add image to articles
    
    * fix: rename image fetcher file
    
    * feat: add graph page views
    
    * fix: dependencies
    
    * fix: dependencies
    
    * fix: dependencies
    
    ---------
    
    Co-authored-by: Thierry CH <thierrybakera12@gmail.com>

--- src/helpers/fetchdata.js
+++ src/helpers/fetchdata.js
@@ -8,9 +8,7 @@ const axiosInstance = axios.create({
 
 const fetchMostVisitedArticlesData = async ({ country, access, year, month, day }) => {
     try {
-        const formattedMonth = month.padStart(2, '0');
-        const formattedDay = day.padStart(2, '0');
-        const apiUrl = `https://wikimedia.org/api/rest_v1/metrics/pageviews/top-per-country/${country}/${access}/${year}/${formattedMonth}/${formattedDay}`;
+        const apiUrl = `https://wikimedia.org/api/rest_v1/metrics/pageviews/top-per-country/${country}/${access}/${year}/${month}/${day}`;
 
         return await axiosInstance.get(apiUrl);
     } catch (error) {
