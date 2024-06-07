45ed4cbe3696756c72d36b6d356fa04b3c285432
Gerard <gerardcubakabisimwa@gmail.com>
2024-06-07 15:05:07 +0200

    fix: add skeletton

---
+++ src/components/ArticleCardSkeletton.jsx
@@ -0,0 +1,29 @@
+import './articleCardSkeletton.css';
+
+// Documentatin d'utilisation de skeletton
+// Ce composant prend un props qui est le nombre de fois où nous souhaiter afficher le skeletton
+function afficheSkeletton(nombre) {
+    const skeletton = [];
+    for (let i = 0; i < nombre; i++) {
+        skeletton.push(
+            <div key={i} className='article-cardskeleton'>
+                <div className='article-imageskeleton skeleton'></div>
+                <div className='article-content-skeleton'>
+                    <div className='article-title-skeleton skeleton'></div>
+                    <div className='article-author skeleton'></div>
+                    <div className='article-date skeleton'></div>
+                </div>
+            </div>,
+        );
+    }
+    return skeletton;
+}
+
+const ArticleCardSkeletton = ({ nombre }) => {
+    return (
+        <>
+            <div className='articles-gallery'>{afficheSkeletton(nombre)}</div>
+        </>
+    );
+};
+export default ArticleCardSkeletton;
