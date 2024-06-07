da5a5d32a085ad0c1cb8a02ed4a4590fc16f1ef6
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-06-07 11:54:02 +0200

    Add button (#132)
    * feat(ArticleCard): use Button component in ArticleCard
    
    * feat(ArticleCard): use Button component in ArticleCard
    
    * feat(ArticleCard): use Button component in ArticleCard

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import { useState } from 'react';
 import ResultListArticles from './components/ResultListArticles.jsx';
 import TopVisited from './components/TopVisited.jsx';
 import ResultatGallery from './components/ResultatGallery.jsx';
+import ArticleCard from './components/ArticleCard.jsx';
 
 function App() {
     const titre = 'Pays';
@@ -32,8 +33,8 @@ function App() {
             <button onClick={handleClicked}>Toggle Articles/Gallery</button>
 
             {showArticles ? <ResultListArticles /> : <ResultatGallery />}
-            {/* <h2>Most visited wikimedia articles</h2> */}
-            {/* <ResultatGallery /> */}
+            <ArticleCard />
+
             <TopVisited />
             <SetDate />
             <Menu data={datab} title={titre} />
