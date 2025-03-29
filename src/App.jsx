a69c13041ed7e8af9dc77d18bccff25fd92a6b87
balavane <danielbashonga88@gmail.com>
2025-03-29 18:37:23 +0100

    feat: ajouter un fond aléatoire dynamique

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Button from './components/Button.jsx';
 import ExportDropdownButton from './components/DropdownExport.jsx';
 import { Menu } from './components/Menu';
 import { datab } from '../data';
-
 import { useState } from 'react';
 import TopVisited from './components/TopVisited.jsx';
 import WikiAfricaTopArticles from './components/africanArticlesList.jsx';
 import { Footer } from './components/footer.jsx';
+import Background from './components/backgr.jsx'; // Importez le composant Background
 
 function App() {
     const titre = 'Pays';
@@ -17,14 +17,18 @@ function App() {
     const handleClicked = () => {
         setShowArticles(!showArticles);
     };
+
     return (
-        <div>
-            <div className='container mx-auto py-4'>
+        <Background>
+            {' '}
+            {/* Encapsulez tout votre contenu avec le Background */}
+            <div className='container mx-auto py-4 relative z-10'>
+                {' '}
+                {/* Ajoutez relative z-10 pour que le contenu apparaisse au-dessus du fond */}
                 <div className='flex align-center justify-center py-8'>
                     <ExportDropdownButton />
                     <button onClick={handleClicked}>Toggle Articles/Gallery</button>
                 </div>
-
                 <TopVisited />
                 <SetDate />
                 <Menu data={datab} title={titre} />
@@ -32,8 +36,9 @@ function App() {
                 <Button text='submit' event={handleClick}></Button>
                 <WikiAfricaTopArticles />
             </div>
-            <Footer />
-        </div>
+            <Footer className='relative z-10' /> {/* Assurez-vous que le footer est aussi au-dessus du fond */}
+        </Background>
     );
 }
+
 export default App;
